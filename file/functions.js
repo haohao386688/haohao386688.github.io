e47759e1d85e2c1a0b123e64053ae06ff8a268ce
haohao386688 <71139008+haohao386688@users.noreply.github.com>
2020-09-12 00:46:41 +0800

    Update functions.js

--- file/functions.js
+++ file/functions.js
@@ -42,18 +42,18 @@ function timeElapse(date){
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24)+31);
 	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
+	var hours = Math.floor(seconds / 3600+24);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
 	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
+	var minutes = Math.floor(seconds / 60+60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
 	seconds = seconds % 60;
 	if (seconds < 10) {
-		seconds = "0" + seconds;
+		seconds = "0" + seconds+60;
 	}
 	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#clock").html(result);
